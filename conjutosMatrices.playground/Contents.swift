336adbb926c3ee48f611d4d003fafeb188ebc57a
eduardoquintero117 <eduardoquiju@hotmail.com>
2022-02-02 14:28:38 -0600

    problema a desarrollar

---
+++ conjutosMatrices.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Problema : Tenemos una matriz compuesta por letras que si son iguales horizontalmente o verticalmente perteneces al mismo conjunto.Determinar el numero de conjuntos
+
+
+// entrada
+//var pict : [String] = ["aabba","aabba","aaacb"]
+var pict : [String] = ["bbba","abba","acaa","aaab"]
+
+//funcion
+func numeroConjuntos(picture: [String] ) {
+    
+}
+// salida
+numeroConjuntos(picture: pict)
+// para ["bbba","abba","acaa","aaab"] -> 4
