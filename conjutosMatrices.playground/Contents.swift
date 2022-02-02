b2e32bfed8f196bdb9557075cafd8a5d2006c0a6
eduardoquintero117 <eduardoquiju@hotmail.com>
2022-02-02 16:10:53 -0600

    se agregaron las variables

--- conjutosMatrices.playground/Contents.swift
+++ conjutosMatrices.playground/Contents.swift
@@ -15,6 +15,21 @@ func numeroConjuntos(picture: [String] ) {
         matris = picture.map({$0.map({$0})})
         print(matris)
     
+    // variables para nuevos conjuntos-----------------------------
+        var cont = 0
+        var r = 0
+        var c = 0
+        var vector = [(r,c)]
+        var cambio = [(Int,Int)]()
+        var cambio2 = [(Int,Int)]()
+        var color = [[(Int,Int)]]()
+        var total = matris.count * matris[0].count
+        var colorTotal = 0
+        var state = true
+    
+    
+    
+    
 }
 // salida
 numeroConjuntos(picture: pict)
