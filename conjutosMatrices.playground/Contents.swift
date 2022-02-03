c15eda9fe4efdb6f0dc91a6880285df90ac327b0
eduardoquintero117 <eduardoquiju@hotmail.com>
2022-02-02 23:15:15 -0600

    filtra y crea conjuntos primera solucion

--- conjutosMatrices.playground/Contents.swift
+++ conjutosMatrices.playground/Contents.swift
@@ -5,7 +5,9 @@ import UIKit
 
 // entrada
 //var pict : [String] = ["aabba","aabba","aaacb"]
-var pict : [String] = ["bbba","abba","acaa","aaab"]
+//var pict : [String] = ["bbba","abba","acaa","aaab"]
+//var pict : [String] = ["abbc","bbbc","acac","ccba"]
+var pict : [String] = ["aaab","bbba","aabc","bcab"]
 
 //funcion
 func numeroConjuntos(picture: [String] ) {
@@ -27,6 +29,203 @@ func numeroConjuntos(picture: [String] ) {
         var colorTotal = 0
         var state = true
     
+    
+    while state {
+            
+            if(colorTotal > total){state = false}
+            if(colorTotal ==  total){state = false}
+            
+            colorTotal = 0
+            r = vector[cont].0
+            c = vector[cont].1
+            
+            //.-
+            if  c < matris[r].count - 1 {
+                if matris[r][c] == matris[r][c+1] {
+                    var bool = true
+                    for i in vector{
+                        if i == (r,c+1){bool = false}
+                    }
+                    if bool {vector.append((r,c+1))}
+                    
+                }else{
+                    //print("NO \((r,c+1))")
+                    cambio.append((r,c+1))
+                }
+                
+                
+            }
+            
+            //.-
+            
+            //-------------------------atras1
+            if ( c > 0) {
+                //.-
+                
+                if matris[r][c] == matris[r][c-1] {
+                    var bool = true
+                    for i in vector{
+                        if i == (r,c-1){bool = false}
+                    }
+                    if bool {vector.append((r,c-1))}
+                    
+                }else{
+                    //print("NO \((r,c+1))")
+                    cambio.append((r,c-1))
+                }
+                
+                
+                
+                
+                //.-
+            }
+            //-------------------------atras2
+            
+            //------------------ arriba 1
+            
+            if ( r > 0) {
+                if (matris[r][c] == matris[r-1][c] ){
+                    var bool = true
+                    for i in vector{
+                        if i == (r-1,c){bool = false}
+                    }
+                    if bool { vector.append((r-1,c))}
+                    
+                }else{
+                    //print("NOp \((r+1,c))")
+                    cambio.append((r-1,c))
+                }
+                
+                
+            }
+            
+            
+            
+            
+            //---------------- arriba 2
+            //.-.-
+            if ( r < matris.count - 1) {
+                if (matris[r][c] == matris[r+1][c] ){
+                    var bool = true
+                    for i in vector{
+                        if i == (r+1,c){bool = false}
+                    }
+                    if bool { vector.append((r+1,c))}
+                    
+                }else{
+                    //print("NOp \((r+1,c))")
+                    cambio.append((r+1,c))
+                }
+                
+                
+            }
+            //.-.-
+       
+        
+            cont = cont + 1
+            
+        
+        
+            if vector.count == cont {
+               
+                
+                //elementos guardados para siguientes iteraciones--
+                //print(color)
+                //print("\(vector)------vector")
+                //print("\(cambio)------cambio1")
+                //print("\(cambio2)------cambio2")
+                //----------------------------------
+                
+                color.append(vector)
+                
+                if(cambio.count == 0){state = false}
+                vector.removeAll()
+                
+                //---- filtro-------------
+                if (cambio2.count > 0 ){
+                    var tempArray = [(Int,Int)]()
+                    for col in color {
+                        for i1 in col {
+                            for i2 in cambio2  {
+                                if i1 == i2 {tempArray.append(i1)}
+                                
+                            }
+                            colorTotal += 1
+                        }
+                    }
+                    //elemtos repetidos
+                    //print(tempArray)
+                    print(colorTotal)
+                    for i in tempArray  {
+                        cambio2 = cambio2.filter { $0 != i}
+                    }
+                    
+                    //print("\(cambio2)------cambio2")
+                    
+                    colorTotal = 0
+                    tempArray.removeAll()
+                    
+                    for col in color {
+                        for i1 in col {
+                            for i2 in cambio  {
+                                if i1 == i2 {tempArray.append(i1)}
+                                
+                            }
+                            colorTotal += 1
+                        }
+                    }
+                    
+                    print(colorTotal)
+                    for i in tempArray  {
+                        cambio = cambio.filter { $0 != i}
+                    }
+                    
+                    //print("\(cambio)------cambio")
+                    
+                }
+                
+                
+                
+                
+                //---- filtro-------------
+                
+                
+                
+                if(cambio.count > 0){
+                    
+                    cambio2 = cambio2 + cambio
+                    vector.append(cambio2[0])
+                    cambio2.remove(at: 0)
+                    
+                }
+                
+                if(cambio.count == 0 && cambio2.count > 0){
+                    
+                    
+                    vector.append(cambio2[0])
+                    cambio2.remove(at: 0)
+                    
+                }
+                
+                print(cambio2)
+                cont = 0
+                cambio.removeAll()
+                print("parte 2")
+                print("\(vector)------vector")
+                if(vector.count ==  0){state = false}
+                
+            }
+            
+        
+            
+        }
+    
+        print("final-------------------")
+        
+        for b in 0...color.count - 1  {
+            print(color[b])
+        }
+    print(color.count)
     
     
     
